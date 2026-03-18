53fe99f9cd040d9a82914bdbf0a799afb77ef114
Shashank <shashankvermayoyo@gmail.com>
2026-03-18 09:15:39 +0530

    fix: add clean-up

--- src/main.c
+++ src/main.c
@@ -7,34 +7,61 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *get_expression_from_file(const char *file_name);
+/* ----- DECLARATIONS ----- */
+char *get_source_from_file(const char *file_name);
 
+// TODO: create readme, better error reporting
 int main() {
-	const char *expression = get_expression_from_file("example/mix.chalk");
-	lexer_t *lexer = lexer_new(expression);
-	ast_node_t *tree = parse_expression(lexer);
+	FILE *out_lexer = fopen("tests/output_lexer.txt", "w");
+	FILE *out_parser = fopen("tests/output_parser.txt", "w");
+	FILE *out_program = fopen("tests/output_program.txt", "w");
 	
-	printf("----- Expression -----\n");
-	printf("%s\n", expression);
+	if (!out_lexer) {
+		die("main(): null out_lexer");
+	}
 	
-	printf("----- Result -----\n");
-	printf("%g\n", ast_eval(tree));
+	if (!out_parser) {
+		die("main(): null out_parser");
+	}
 	
-	printf("----- AST -----\n");
-	ast_dump(tree, 0);
+	if (!out_program) {
+		die("main(): null out_program");
+	}
+	
+	char *source_program = get_source_from_file("example/mix.chalk");
+	lexer_t *lexer = lexer_new(source_program);
+	ast_node_t *program_tree = parse_program(lexer);
+
+	printf("----- Program -----\n");
+	printf("%s\n", source_program);
+
+	printf("----- Output -----\n");
+	ast_eval(program_tree, stdout);
+	
+	// printf("----- AST -----\n");
+	// ast_dump(program_tree, stdout);
+
+	/* clean-up */
+	fclose(out_lexer);
+	fclose(out_parser);
+	fclose(out_program);
+	free(source_program);
+	lexer_free(&lexer);
 	
 	return 0;
 }
 
-char *get_expression_from_file(const char *file_name) {
+/* ----- DEFINITIONS ----- */
+
+char *get_source_from_file(const char *file_name) {
 	if (!file_name) {
-		die("get_expression_from_file(): null file name");
+		die("get_source_from_file(): null file name");
 	}
 
 	FILE *file = fopen(file_name, "r");
 
 	if (!file) {
-		die("get_expression_from_file(): cannot open file");
+		die("get_source_from_file(): cannot open file");
 	}
 	
 	fseek(file, 0, SEEK_END); /* set file cursor at EOF */
@@ -45,6 +72,8 @@ char *get_expression_from_file(const char *file_name) {
 	char *buff = mem_alloc(sizeof(char) * file_size + 1);
 	fread(buff, sizeof(char), file_size, file);
 	buff[file_size] = '\0';
+
+	fclose(file);
 
 	return buff;
 }
